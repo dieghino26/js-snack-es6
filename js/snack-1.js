d81d8be58fffba068719332b3a4929f6ac23ae57
dieghino26 <brotzadiego@gmail.com>
2024-01-18 23:48:16 +0100

    finisced first snack

--- js/snack-1.js
+++ js/snack-1.js
@@ -16,3 +16,24 @@
 // ];
 // Per stampare il tutto, la tipografia  vuole che le mandiamo una lista di ospiti in cui ogni ospite sia un oggetto javascript che ha come attributi: nome del tavolo, nome dell'ospite e posto occupato.
 // Generiamo questo array in una variabile guestsList e stampiamolo in console
+
+
+const table = 'Tavolo Vip';
+
+const guests = [
+    'Brad Pitt',
+    'Johnny Depp',
+    'Lady Gaga',
+    'Cristiano Ronaldo',
+    'Georgina Rodriguez',
+    'Chiara Ferragni',
+    'Fedez',
+    'George Clooney',
+    'Amal Clooney',
+];
+
+
+const list = guests.map((name, i) => ({ table, name, place: i + 1 }));
+
+
+console.log(list);
