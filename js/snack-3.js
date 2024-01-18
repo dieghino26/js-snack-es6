bb0d54ac16a6d7538c5ad1b1f53ef4075e88921e
dieghino26 <brotzadiego@gmail.com>
2024-01-18 23:43:09 +0100

    added other snacks

---
+++ js/snack-3.js
@@ -0,0 +1,1 @@
+//Creare un array di oggetti: Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: name e peso. Stampare in console la bici con peso minore utilizzando il destructuring
